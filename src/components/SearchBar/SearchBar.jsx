da31d914b6d6efccf905e1a08b619d7cbf07f738
andreas <andr493i@elev.techcollege.dk>
2025-09-19 12:22:50 +0200

    sisdte rattle

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -12,13 +12,13 @@ export function SearchBar() {
 
     return (
         <div>
-            <div className="sticky top-0 bg-white shadow p-4 flex items-center space-x-2 z-50">
+            <div className="sticky top-0 bg-white shadow p-4 flex items-center space-x-2">
                 <input value={From} onChange={(e) => setFrom(e.target.value)} placeholder="hvor fra?" className="border p-2 flex-1 rounded" />
             </div>
 
-            <button onClick={swapPlaces} className="px-3 py-2 bg-grey-200 rounded hover:bg-grey-300 font-bold text-4xl z-50">↔︎</button>
+            <button onClick={swapPlaces} className="px-3 py-2 bg-grey-200 rounded hover:bg-grey-300 font-bold text-4xl">↔︎</button>
 
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 z-50">Søg lift</button>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Søg lift</button>
         </div>
     );
 }
